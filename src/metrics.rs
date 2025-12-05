8160b38541d1dd172e05d81e91e32917bb64a026
perl_d <david.perl@psi.ch>
2025-12-05 13:50:25 +0100

    wip2

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,32 +1,61 @@
-use crate::config::MetricsConfig;
+use crate::{
+    config::{MetricsConfig, RedisConfig},
+    redis_logs::create_redis_conn,
+};
 use futures::future::join_all;
+use redis::{Commands, Connection};
 use std::collections::HashMap;
 use sysinfo::System;
-use tokio::{spawn, sync::mpsc, task::JoinHandle, time};
+use tokio::{
+    spawn,
+    sync::mpsc::{self, UnboundedSender},
+    task::JoinHandle,
+    time,
+};
 
 type SimpleMetricFunc = Box<dyn Fn() -> String + Send + Sync>;
 type SysinfoMetricFunc = Box<dyn Fn(&System) -> String + Send + Sync>;
-type RedisMetricFunc = Box<dyn Fn(i32) -> String + Send + Sync>;
+type RedisMetricFunc = Box<dyn Fn(&mut Connection) -> String + Send + Sync>;
+type MetricUpdate = (String, String);
+
 enum MetricFunc {
     Simple(SimpleMetricFunc),
     Redis(RedisMetricFunc),
     System(SysinfoMetricFunc),
 }
 
-async fn metric_future(metric_func: MetricFunc, mut interval: time::Interval) {
-    let system = System::new_all();
-    loop {
-        interval.tick().await;
-
-        match metric_func {
-            MetricFunc::Simple(ref func) => func(),
-            MetricFunc::Redis(ref func) => {
-                let redis: i32 = 5;
-                func(redis)
+async fn metric_future(
+    name: String,
+    metric_func: MetricFunc,
+    mut interval: time::Interval,
+    redis_config: RedisConfig,
+    tx: UnboundedSender<MetricUpdate>,
+) {
+    match metric_func {
+        MetricFunc::Simple(ref func) => loop {
+            if tx.send(("".into(), func())).is_err() {
+                break;
             }
-            MetricFunc::System(ref func) => func(&system),
-        };
-    }
+        },
+        MetricFunc::Redis(ref func) => {
+            let mut redis = create_redis_conn(&redis_config.url.full_url())
+                .expect("Could not connect to Redis!");
+            loop {
+                if tx.send(("".into(), func(&mut redis))).is_err() {
+                    break;
+                }
+            }
+        }
+        MetricFunc::System(ref func) => {
+            let system = System::new_all();
+            loop {
+                interval.tick().await;
+                if tx.send(("".into(), func(&system))).is_err() {
+                    break;
+                }
+            }
+        }
+    };
 }
 
 macro_rules! system_metric {
@@ -39,7 +68,15 @@ macro_rules! system_metric {
         )
     };
 }
-
+macro_rules! redis_metric {
+    ($func:expr) => {
+        (
+            stringify!($func).to_string(),
+            $crate::metrics::MetricFunc::Redis(Box::new($func) as $crate::metrics::RedisMetricFunc),
+        )
+    };
+}
+// System info metrics
 fn cpu_usage_percent(system: &System) -> String {
     system.global_cpu_usage().to_string()
 }
@@ -50,19 +87,41 @@ fn ram_available_bytes(system: &System) -> String {
     system.available_memory().to_string()
 }
 
-pub async fn metrics_loop(config: MetricsConfig) {
-    let (tx, mut rx) = mpsc::unbounded_channel::<String>();
+// Metrics from redis
+fn redis_metric_1(redis: &mut Connection) -> String {
+    if let Ok(res) = redis.get::<&str, String>("key") {
+        res
+    } else {
+        "ERROR".into()
+    }
+}
+
+pub async fn metrics_loop(config: MetricsConfig, redis_config: RedisConfig) {
+    let (tx, mut rx) = mpsc::unbounded_channel::<MetricUpdate>();
 
     let metrics: HashMap<String, MetricFunc> = HashMap::from([
+        // System info metrics
         system_metric!(cpu_usage_percent),
         system_metric!(ram_usage_bytes),
         system_metric!(ram_available_bytes),
+        // Redis metrics
+        redis_metric!(redis_metric_1),
     ]);
 
     let futs: Vec<JoinHandle<()>> = metrics
         .into_iter()
-        .map(|(name, func)| spawn(metric_future(func, config.interval_for_metric(&name))))
+        .map(|(name, func)| {
+            let interval = config.interval_for_metric(&name);
+            spawn(metric_future(
+                name,
+                func,
+                interval,
+                redis_config.clone(),
+                tx.clone(),
+            ))
+        })
         .collect();
+
     join_all(futs).await;
 }
 
