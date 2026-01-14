95eb7bec12d6588cf154dff865a05ad9bbabb2de
David Perl <david.perl@psi.ch>
2026-01-14 14:49:44 +0100

    ci: modify build script a bit

--- build.rs
+++ build.rs
@@ -6,21 +6,23 @@ use which::which;
 
 fn main() -> Result<()> {
     println!("Build setup for bec_log_ingestor.");
-    print!("Assuring availability of protoc...");
+    print!("Assuring availability of protoc... ");
     let protoc_exists = which("protoc");
     if protoc_exists.is_err() {
         println!("protoc not found.");
         if consts::OS != "linux" {
             println!("Auto-fetching of protoc is only intended for use in CI, install it manually.")
         } else {
-            dbg!(_ = Command::new("sudo").arg("apt-get").arg("update").output());
-            dbg!(
-                _ = Command::new("sudo")
-                    .arg("apt-get")
-                    .arg("install")
-                    .arg("protobuf-compiler")
-                    .output()
-            );
+            let res = Command::new("sudo").arg("apt-get").arg("update").output();
+            dbg!(&res);
+            if res.is_err() {};
+            let res = Command::new("sudo")
+                .arg("apt-get")
+                .arg("install")
+                .arg("protobuf-compiler")
+                .output();
+            dbg!(&res);
+            if res.is_err() {};
         }
     };
     prost_build::compile_protos(&["src/prometheus_write.proto"], &["src/"])?;
