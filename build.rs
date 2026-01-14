cd32b50480569b2495c27889e560b5a8ebd6c659
David Perl <david.perl@psi.ch>
2026-01-14 14:34:28 +0100

    fix: improve build rs

--- build.rs
+++ build.rs
@@ -5,17 +5,22 @@ use std::process::Command;
 use which::which;
 
 fn main() -> Result<()> {
+    println!("Build setup for bec_log_ingestor.");
+    print!("Assuring availability of protoc...");
     let protoc_exists = which("protoc");
     if protoc_exists.is_err() {
+        println!("protoc not found.");
         if consts::OS != "linux" {
             println!("Auto-fetching of protoc is only intended for use in CI, install it manually.")
         } else {
-            _ = Command::new("sudo").arg("apt-get").arg("update").output();
-            _ = Command::new("sudo")
-                .arg("apt-get")
-                .arg("install")
-                .arg("protobuf-compiler")
-                .output();
+            dbg!(_ = Command::new("sudo").arg("apt-get").arg("update").output());
+            dbg!(
+                _ = Command::new("sudo")
+                    .arg("apt-get")
+                    .arg("install")
+                    .arg("protobuf-compiler")
+                    .output()
+            );
         }
     };
     prost_build::compile_protos(&["src/prometheus_write.proto"], &["src/"])?;
